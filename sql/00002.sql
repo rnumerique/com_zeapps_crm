00aadeea98b6af7be46cc89481941114f0aa53e9
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-25 14:23:41 +0200

    updated sql changes file

--- sql/00002.sql
+++ sql/00002.sql
@@ -5,12 +5,19 @@ ALTER TABLE `zeapps_quote_documents` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000
 
 ALTER TABLE `zeapps_quote_activities` ADD `id_user` INT UNSIGNED NOT NULL AFTER `id_quote`, ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_quote_activities` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000-00-00' AFTER `description`;
+ALTER TABLE `zeapps_quote_activities` CHANGE `deadline` `deadline` TIMESTAMP NOT NULL;
+ALTER TABLE `zeapps_quote_activities` ADD `status` VARCHAR(255) NOT NULL AFTER `description`, ADD `id_type` INT UNSIGNED NOT NULL AFTER `status`, ADD `label_type` VARCHAR(255) NOT NULL AFTER `id_type`;
 
 ALTER TABLE `zeapps_quotes` ADD `name_contact` VARCHAR(255) NOT NULL AFTER `id_contact`;
 ALTER TABLE `zeapps_quotes` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_company`;
 ALTER TABLE `zeapps_quotes` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_quotes` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_quotes` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ALTER TABLE `zeapps_quote_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
+ALTER TABLE `zeapps_quote_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
+ALTER TABLE `zeapps_quote_lines` ADD `accounting_number` VARCHAR(255) NOT NULL AFTER `value_taxe`;
+ALTER TABLE `zeapps_quote_lines` ADD `ref` VARCHAR(255) NOT NULL AFTER `id_product`;
 
 
 
@@ -22,12 +29,19 @@ ALTER TABLE `zeapps_invoice_documents` ADD `date` TIMESTAMP NOT NULL DEFAULT '00
 
 ALTER TABLE `zeapps_invoice_activities` ADD `id_user` INT UNSIGNED NOT NULL AFTER `id_invoice`, ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_invoice_activities` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000-00-00' AFTER `description`;
+ALTER TABLE `zeapps_invoice_activities` CHANGE `deadline` `deadline` TIMESTAMP NOT NULL;
+ALTER TABLE `zeapps_invoice_activities` ADD `status` VARCHAR(255) NOT NULL AFTER `description`, ADD `id_type` INT UNSIGNED NOT NULL AFTER `status`, ADD `label_type` VARCHAR(255) NOT NULL AFTER `id_type`;
 
 ALTER TABLE `zeapps_invoices` ADD `name_contact` VARCHAR(255) NOT NULL AFTER `id_contact`;
 ALTER TABLE `zeapps_invoices` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_company`;
 ALTER TABLE `zeapps_invoices` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_invoices` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_invoices` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ALTER TABLE `zeapps_invoice_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
+ALTER TABLE `zeapps_invoice_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
+ALTER TABLE `zeapps_invoice_lines` ADD `accounting_number` VARCHAR(255) NOT NULL AFTER `value_taxe`;
+ALTER TABLE `zeapps_invoice_lines` ADD `ref` VARCHAR(255) NOT NULL AFTER `id_product`;
 
 
 
@@ -40,12 +54,19 @@ ALTER TABLE `zeapps_order_documents` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000
 
 ALTER TABLE `zeapps_order_activities` ADD `id_user` INT UNSIGNED NOT NULL AFTER `id_order`, ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_order_activities` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000-00-00' AFTER `description`;
+ALTER TABLE `zeapps_order_activities` CHANGE `deadline` `deadline` TIMESTAMP NOT NULL;
+ALTER TABLE `zeapps_order_activities` ADD `status` VARCHAR(255) NOT NULL AFTER `description`, ADD `id_type` INT UNSIGNED NOT NULL AFTER `status`, ADD `label_type` VARCHAR(255) NOT NULL AFTER `id_type`;
 
 ALTER TABLE `zeapps_orders` ADD `name_contact` VARCHAR(255) NOT NULL AFTER `id_contact`;
 ALTER TABLE `zeapps_orders` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_company`;
 ALTER TABLE `zeapps_orders` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_orders` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_orders` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ALTER TABLE `zeapps_order_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
+ALTER TABLE `zeapps_order_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
+ALTER TABLE `zeapps_order_lines` ADD `accounting_number` VARCHAR(255) NOT NULL AFTER `value_taxe`;
+ALTER TABLE `zeapps_order_lines` ADD `ref` VARCHAR(255) NOT NULL AFTER `id_product`;
 
 
 
@@ -57,9 +78,32 @@ ALTER TABLE `zeapps_delivery_documents` ADD `date` TIMESTAMP NOT NULL DEFAULT '0
 
 ALTER TABLE `zeapps_delivery_activities` ADD `id_user` INT UNSIGNED NOT NULL AFTER `id_delivery`, ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_delivery_activities` ADD `date` TIMESTAMP NOT NULL DEFAULT '0000-00-00' AFTER `description`;
+ALTER TABLE `zeapps_delivery_activities` CHANGE `deadline` `deadline` TIMESTAMP NOT NULL;
+ALTER TABLE `zeapps_delivery_activities` ADD `status` VARCHAR(255) NOT NULL AFTER `description`, ADD `id_type` INT UNSIGNED NOT NULL AFTER `status`, ADD `label_type` VARCHAR(255) NOT NULL AFTER `id_type`;
 
 ALTER TABLE `zeapps_deliveries` ADD `name_contact` VARCHAR(255) NOT NULL AFTER `id_contact`;
 ALTER TABLE `zeapps_deliveries` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_company`;
 ALTER TABLE `zeapps_deliveries` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_deliveries` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_deliveries` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+ALTER TABLE `zeapps_delivery_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
+ALTER TABLE `zeapps_delivery_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
+ALTER TABLE `zeapps_delivery_lines` ADD `accounting_number` VARCHAR(255) NOT NULL AFTER `value_taxe`;
+ALTER TABLE `zeapps_delivery_lines` ADD `ref` VARCHAR(255) NOT NULL AFTER `id_product`;
+
+
+
+
+
+CREATE TABLE `zeapps_crm_activities` (
+  `id` int(10) unsigned NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `zeapps_crm_activities` ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `zeapps_crm_activities` MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
