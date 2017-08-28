b31484089fe87a4160e635b3a0f7d943a7602296
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-28 12:04:24 +0200

    updated sql commands

--- sql/00002.sql
+++ sql/00002.sql
@@ -13,6 +13,8 @@ ALTER TABLE `zeapps_quotes` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_c
 ALTER TABLE `zeapps_quotes` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_quotes` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_quotes` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_quotes` CHANGE `modalities` `label_modality` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_quotes` ADD `id_modality` INT UNSIGNED NOT NULL AFTER `date_limit`;
 
 ALTER TABLE `zeapps_quote_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
 ALTER TABLE `zeapps_quote_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
@@ -37,6 +39,8 @@ ALTER TABLE `zeapps_invoices` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id
 ALTER TABLE `zeapps_invoices` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_invoices` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_invoices` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_invoices` CHANGE `modalities` `label_modality` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_invoices` ADD `id_modality` INT UNSIGNED NOT NULL AFTER `date_limit`;
 
 ALTER TABLE `zeapps_invoice_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
 ALTER TABLE `zeapps_invoice_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
@@ -62,6 +66,8 @@ ALTER TABLE `zeapps_orders` ADD `name_company` VARCHAR(255) NOT NULL AFTER `id_c
 ALTER TABLE `zeapps_orders` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_orders` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_orders` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_orders` CHANGE `modalities` `label_modality` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_orders` ADD `id_modality` INT UNSIGNED NOT NULL AFTER `date_limit`;
 
 ALTER TABLE `zeapps_order_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
 ALTER TABLE `zeapps_order_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
@@ -86,12 +92,13 @@ ALTER TABLE `zeapps_deliveries` ADD `name_company` VARCHAR(255) NOT NULL AFTER `
 ALTER TABLE `zeapps_deliveries` ADD `name_user` VARCHAR(255) NOT NULL AFTER `id_user`;
 ALTER TABLE `zeapps_deliveries` ADD `total_tva` FLOAT(9,2) NOT NULL AFTER `total_ht`;
 ALTER TABLE `zeapps_deliveries` CHANGE `name_user` `name_user_account_manager` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_deliveries` CHANGE `modalities` `label_modality` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `zeapps_deliveries` ADD `id_modality` INT UNSIGNED NOT NULL AFTER `date_limit`;
 
 ALTER TABLE `zeapps_delivery_lines` ADD `id_taxe` INT UNSIGNED NOT NULL AFTER `price_unit`;
 ALTER TABLE `zeapps_delivery_lines` CHANGE `taxe` `value_taxe` FLOAT(4,2) NOT NULL;
 ALTER TABLE `zeapps_delivery_lines` ADD `accounting_number` VARCHAR(255) NOT NULL AFTER `value_taxe`;
 ALTER TABLE `zeapps_delivery_lines` ADD `ref` VARCHAR(255) NOT NULL AFTER `id_product`;
-
 
 
 
@@ -103,7 +110,9 @@ CREATE TABLE `zeapps_crm_activities` (
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 ALTER TABLE `zeapps_crm_activities` ADD PRIMARY KEY (`id`);
-
 ALTER TABLE `zeapps_crm_activities` MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+
+
+
+ALTER TABLE `zeapps_users` ADD `id_warehouse` INT UNSIGNED NOT NULL;
