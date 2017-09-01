8bed73ce6978eb9ac12c506f8afc7c6732ea7f09
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-01 17:20:19 +0200

    No longer using class type directives

--- views/stock/chart.php
+++ views/stock/chart.php
@@ -20,7 +20,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div class="row">
     <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-        <canvas id="base" class="chart-line"
+        <canvas id="base" chart-line
+                class="chart-line"
                 chart-data="data"
                 chart-labels="labels">
         </canvas>
