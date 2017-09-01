80924e168e26e86cdbb53da731f2bf214511f308
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-01 17:24:59 +0200

    corrected function call to fit our needs (limit & offset)

--- models/Zeapps_stocks.php
+++ models/Zeapps_stocks.php
@@ -1,12 +1,13 @@
 <?php
 class Zeapps_stocks extends ZeModel {
-    public function all($where = array()){
+    public function all($where = array(), $limit = 2147483647, $offset = 0){
 
         $where['deleted_at'] = null;
 
         return $this->database()->select('*,
                                         sum(total) as total')
                                 ->where($where)
+                                ->limit($limit, $offset)
                                 ->group_by('id_stock')
                                 ->table('zeapps_stocks')
                                 ->result();
