fef7cd8a4bffe839fa6f4cdfeffd26832b12ba0c
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-13 09:38:03 +0200

    removed unnecessary columns

--- views/orders/lists_partial.php
+++ views/orders/lists_partial.php
@@ -32,8 +32,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <th>Date de création</th>
                     <th>Date limite</th>
                     <th>Responsable</th>
-                    <th class="text-right">%</th>
-                    <th>Statut</th>
                     <th></th>
                 </tr>
                 </thead>
@@ -53,8 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <td ng-click="goTo(order.id)">{{order.date_creation | date:'dd/MM/yyyy'}}</td>
                     <td ng-click="goTo(order.id)">{{order.date_limit | date:'dd/MM/yyyy'}}</td>
                     <td ng-click="goTo(order.id)">{{order.name_user_account_manager}}</td>
-                    <td ng-click="goTo(order.id)" class="text-right">{{order.probability | number:2}}</td>
-                    <td ng-click="goTo(order.id)">{{order.status}}</td>
                     <td class="text-right">
                         <ze-btn fa="pencil" color="info" direction="left" hint="Editer"
                                 ze-modalform="edit"
