b1a2a4d76fff037604d658e23853f69d51bbf0fb
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-09-15 12:01:24 +0200

    added model source

--- models/Zeapps_product_products.php
+++ models/Zeapps_product_products.php
@@ -2,21 +2,21 @@
 class Zeapps_product_products extends ZeModel {
 
     public function insert($data = array()){
-        $this->_pLoad->model("Zeapps_product_categories", "categories");
+        $this->_pLoad->model("Zeapps_product_categories", "categories", "com_zeapps_crm");
 
-        //$this->_pLoad->ctrl->categories->newProductIn($data['id_cat']);
+        $this->_pLoad->ctrl->categories->newProductIn($data['id_cat']);
 
         return parent::insert($data);
     }
 
     public function update($data = array(), $where = array()){
-        $this->_pLoad->model("Zeapps_product_categories", "categories");
+        $this->_pLoad->model("Zeapps_product_categories", "categories", "com_zeapps_crm");
 
         $product = parent::get($where);
 
         if($product->id_cat !== $data['id_cat']) {
-            //$this->_pLoad->ctrl->categories->newProductIn($data['id_cat']);
-            //$this->_pLoad->ctrl->categories->removeProductIn($product->id_cat);
+            $this->_pLoad->ctrl->categories->newProductIn($data['id_cat']);
+            $this->_pLoad->ctrl->categories->removeProductIn($product->id_cat);
         }
 
         return parent::update($data, $where);
